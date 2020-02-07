c78f25e547871493f67f389606c6ca54818b810d
Ben Hernandez <benjammin234@gmail.com>
2020-02-07 14:01:46 -0800

    Creates header

---
+++ src/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.css";
+
+export default class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1 className="Header__banner">Timeline</h1>
+      </div>
+    );
+  }
+}
